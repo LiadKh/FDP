6912ed26a3683be85d3a9c25717e033c0e20517f
Liad Khamdadash <liadkh95@gmail.com>
2019-11-16 17:55:26 +0200

    environment variables
    add supports to environment variables  if the server doesn't run in production environment

--- server/app.js
+++ server/app.js
@@ -3,6 +3,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+if (process.env.NODE_ENV !== 'production') {
+  const result = require('dotenv').config({
+    path: path.resolve(process.cwd(), './config/environment', `${process.env.NODE_ENV}.env`)
+  })
+
+  if (result.error) {
+    throw result.error
+  }
+
+  console.log(result.parsed)
+}
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -10,7 +22,9 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
