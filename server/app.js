721a07d3809b75c9c86e808182e040d0b90e61ab
Liad Khamdadash <liadkh95@gmail.com>
2019-11-16 22:46:08 +0200

    production build  and server port in development
    add build for production that serve the react app, also change the port of the development server to 5000

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fs = require('fs');
 
-require('../database/mongodb/connection')
+// require('../database/mongodb/connection')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -33,7 +33,7 @@ app.use(logger('common', {
 }))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.cwd(__dirname, '..', 'client', 'build')));
+  app.use(express.static(path.resolve(process.cwd(), 'client/build')));
 }
 
 app.use(express.json());
