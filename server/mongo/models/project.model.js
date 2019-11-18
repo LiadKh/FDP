c41300605f6b76e7631e0bbe34eae9efc7f47293
Liad Khamdadash <liadkh95@gmail.com>
2019-11-19 00:05:27 +0200

    add 3 schema
    add company, user and project schema

---
+++ server/mongo/models/project.model.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var projectSchema = new Schema({
+  name: {
+    type: String,
+    lowercase: true,
+    required: [true, "project name can't be blank"]
+  },
+  technicalEditor: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  managers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  updated: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true,
+});
