484c903dc78d7c4ad76652b06ef1e478dd2db0c4
Liad Khamdadash <liadkh95@gmail.com>
2019-11-30 20:54:30 +0200

    add auth routers and middleware
    add login, logout and logout from all devices.
    also add auth middleware function

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,44 +1,58 @@
-const log = require('debug')('CONNECTION');
+const log = require('debug')('AUTH');
 const express = require('express');
 const User = require('../mongo/models/user');
+const auth = require('../middleware/auth');
+const {
+  HttpErrorHandler,
+} = require('../lib/error');
 
 const router = express.Router();
 
-// router.post('/users', async (req, res) => {
-//   // Create a new user
-//   try {
-//     const user = new User(req.body);
-//     await user.save();
-//     const token = await user.generateAuthToken();
-//     res.status(201).send({
-//       user,
-//       token
-//     });
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
-
-router.post('/login', async (req, res) => {
-  // Login a registered user
+/** login */
+router.post('/login', async (req, res, next) => {
   try {
     const {
       email,
-      password
+      password,
     } = req.body;
+    if (!email || !password) {
+      next(new HttpErrorHandler(400, 'missing email or password'));
+    }
     const user = await User.findByCredentials(email, password);
     if (!user) {
-      return res.status(401).send({
-        error: 'Login failed! Check authentication credentials'
-      });
+      const err = 'Login failed! Check authentication credentials';
+      log(err);
+      next(new HttpErrorHandler(401, err));
     }
     const token = await user.generateAuthToken();
-    res.send({
+    res.status(201).send({
       user,
-      token
+      token,
     });
   } catch (error) {
-    res.status(400).send(error);
+    next(new HttpErrorHandler(400, error));
+  }
+});
+
+/** logout current device */
+router.post('/logout', auth, async (req, res, next) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token);
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    next(new HttpErrorHandler(500, error));
+  }
+});
+
+/** logout out of all device */
+router.post('/logoutAll', auth, async (req, res, next) => {
+  try {
+    req.user.tokens.splice(0, req.user.tokens.length);
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    next(new HttpErrorHandler(500, error));
   }
 });
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const router = express.Router();
 const admin = require('./admin');
 const company = require('./company');
+const auth = require('./auth');
+const user = require('./user');
 
+
+router.use('/', auth);
 router.use('/admin', admin);
+router.use('/user', user);
 router.use('/company', company);
 
 module.exports = router;
